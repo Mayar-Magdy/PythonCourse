a2e0acebfa3a3e16a452843c7cb25b2a55cb1aae
Mayar <maimagdy238@gmail.com>
2024-05-17 15:44:45 +0300

    printing

---
+++ printing.py
@@ -0,0 +1,21 @@
+# when we start with '#' means add a single comment
+"""
+this is a triple comment
+"""
+# comments completely ignored by the interpreter
+
+# print function
+print('mayar')  # mayar
+print("mayar")  # mayar
+
+# \n add a new line
+print('hi, \nhow are you?\n')
+
+# Doing Arithmetics
+print(3 + 1)
+print(5 / 2)
+print(5 % 2)
+
+# comma in printing ',' print spaces
+print("py", "cpp", 3+2)  # py cpp 5
+
